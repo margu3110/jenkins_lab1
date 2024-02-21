28ae3d23d0ef196c6c011b5a96973a25e30d6862
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-02-21 11:30:59 -0300

    This version creates successfully the jenkins server

---
+++ main.tf
@@ -0,0 +1,50 @@
+data "aws_vpc" "default_vpc" {
+  default = true
+} 
+
+#Create security group for EC2 server with Jenkins
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins security group"
+  description = "Allow access to ports 8080 and 22"
+  vpc_id      = data.aws_vpc.default_vpc.id
+
+  ingress {
+    description = "allow all traffic to 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
+    description = "allow all traffic to 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# Terraform Resource Block to create EC2 Jenkins Server
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0e1d30f2c40c4c701"
+  instance_type          = "t2.micro"
+  security_groups        = [aws_security_group.jenkins_sg.name]
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  key_name               = "isildur"
+  user_data              = file("install_jenkins.sh")
+  tags = {
+    Name = "EC2 Jenkins Server"
+  }
+}
