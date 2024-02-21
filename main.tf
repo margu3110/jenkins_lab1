5c6920cd43920e4deb547d4247a811440fc9cb35
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-02-21 11:53:26 -0300

    Creating a S3 bucket to store jenkins artifacts

--- main.tf
+++ main.tf
@@ -48,3 +48,27 @@ resource "aws_instance" "web_server" {
     Name = "EC2 Jenkins Server"
   }
 }
+
+
+resource "aws_s3_bucket" "isildur-tf-jenkins-bucket" {
+  bucket = "isildur-tf-jenkins-bucket-001"
+
+  tags = {
+    Name        = "isildur-tf-jenkins-bucket"
+    Environment = "lab"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "isildur-tf-jenkins-bucket" {
+  bucket = aws_s3_bucket.isildur-tf-jenkins-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "isildur-tf-jenkins-bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.isildur-tf-jenkins-bucket]
+
+  bucket = aws_s3_bucket.isildur-tf-jenkins-bucket.id
+  acl    = "private"
+}
